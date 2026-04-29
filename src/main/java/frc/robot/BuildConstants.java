34c62928c6279a8a70d39d6cabd0aa374125f062
Westley R <regerwestleyj@my.jccmi.edu>
2026-04-29 16:19:32 -0400

    remove VectorKit

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024-Defense-Drive";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 22;
-  public static final String GIT_SHA = "8572081abc2ca181883ab585a2cc1c2efd0e52e1";
-  public static final String GIT_DATE = "2026-04-15 15:46:53 EDT";
+  public static final int GIT_REVISION = 23;
+  public static final String GIT_SHA = "5fd08c02d3e46fc16cf24ba0729a206d039f6aad";
+  public static final String GIT_DATE = "2026-04-29 15:30:17 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-04-29 15:28:52 EDT";
-  public static final long BUILD_UNIX_TIME = 1777490932126L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-04-29 15:31:58 EDT";
+  public static final long BUILD_UNIX_TIME = 1777491118390L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
